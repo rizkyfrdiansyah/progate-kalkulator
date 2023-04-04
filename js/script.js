e5405717bc69870dd900851790c802572e184d39
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-04-04 12:22:44 +0700

    menambahkan argument event

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number) => {
-  console.log(number);
+  number.addEventListener("click", (event) => {
+    console.log(event.target.value);
+  });
 });
