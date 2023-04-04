1fd871f4b1d1abc399205f6462e5c6b94b2e0ebc
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-04-04 13:16:06 +0700

    update fixed di calculationOperator

--- js/script.js
+++ js/script.js
@@ -21,3 +21,51 @@ numbers.forEach((number) => {
     updateScreen(currentNumber);
   });
 });
+
+const operators = document.querySelectorAll(".operator");
+let prevNumber = "";
+let calculationOperator = "";
+
+const inputOperator = (operator) => {
+  prevNumber = currentNumber;
+  calculationOperator = operator;
+  currentNumber = "";
+};
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+  });
+});
+
+const equalSign = document.querySelector(".equal-sign");
+const calculate = () => {
+  let result = "";
+  const prev = parseInt(prevNumber);
+  const current = parseInt(currentNumber);
+  if (isNaN(prev) || isNaN(current)) return;
+
+  switch (calculationOperator) {
+    case "+":
+      result = parseInt(prevNumber) + parseInt(currentNumber);
+      break;
+    case "-":
+      result = parseInt(prevNumber) - parseInt(currentNumber);
+      break;
+    case "*":
+      result = parseInt(prevNumber) * parseInt(currentNumber);
+      break;
+    case "/":
+      result = parseInt(prevNumber) / parseInt(currentNumber);
+      break;
+    default:
+      return;
+  }
+  currentNumber = result.toString();
+  calculationOperator = "";
+};
+
+equalSign.addEventListener("click", () => {
+  calculate();
+  updateScreen(currentNumber);
+});
